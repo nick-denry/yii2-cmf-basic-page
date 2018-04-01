00eb17fc6744f683c6c860e4f763366c5cd92634
Nick Denry <nick.denry@gmail.com>
2018-04-01 16:44:59 +0300

    Change table name to simple_pages
    Add teaser field

--- src/migrations/m180330_163421_create_simple_pages_table.php
+++ src/migrations/m180330_163421_create_simple_pages_table.php
@@ -5,21 +5,22 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `pages`.
  */
-class m180217_103040_create_pages_table extends Migration
+class m180330_163421_create_simple_pages_table extends Migration
 {
     /**
      * @inheritdoc
      */
     public function up()
     {
-        $this->createTable('{{%pages}}', [
+        $this->createTable('{{%simple_pages}}', [
             'id' => $this->primaryKey(),
             'title' => $this->string()->notNull()->comment('Page title'),
+            'teaser' => $this->text()->comment('Page teaser'),
             'body' => $this->text()->notNull()->comment('Page body'),
             'alias' => $this->string()->comment('Page url alias'),
             'is_published' => $this->boolean()->notNull()->defaultValue(true)->comment('Is page published'),
-            'created_at' => $this->timestamp()->notNull()->comment('Page creation time'),
-            'updated_at' => $this->timestamp()->notNull()->comment('Page update time'),
+            'created_at' => $this->datetime()->notNull()->comment('Page creation time'),
+            'updated_at' => $this->datetime()->notNull()->comment('Page update time'),
         ]);
     }
 
