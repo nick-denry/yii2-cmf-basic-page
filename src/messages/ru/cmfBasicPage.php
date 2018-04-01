199bcba01e20bef50cffe92a92aa16f4f0a2e1aa
Nick Denry <nick.denry@gmail.com>
2018-04-01 16:56:00 +0300

    Combine all translations to single file

---
+++ src/messages/ru/cmfBasicPage.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+* Translation map for ru-RU
+*/
+return [
+    'ID' => 'ID',
+    'Title' => 'Заголовок',
+    'Teaser' => 'Анонс',
+    'Body' => 'Содержимое',
+    'Alias' => 'Синоним URL',
+    'Is published' => 'Опубликовано',
+    'Created at' => 'Создано',
+    'Updated at' => 'Обновлено',
+    'Pages' => 'Страницы',
+    'Create page' => 'Добавить страницу'
+];
--- src/messages/ru/pages.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-    'Create'                              => 'Создать',
-    'The requested page does not exist.'  => 'Запрашиваемая страница не найдена.',
-];
